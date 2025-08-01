40c735d9dc3c06633769415b65c6e58cd52ce073
Youssef Kassem <yousef.essa2112@gmail.com>
2025-08-01 03:07:42 -0400

    Replaced DAGs with new test workflows

--- example_dag.py
+++ example_dag.py
@@ -1,21 +1,17 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+def print_hello():
+    print("Hello from new_example_dag!")
+
 with DAG(
-    dag_id="example_dag",
+    dag_id="new_example_dag",
     start_date=datetime(2023, 1, 1),
-    schedule='@daily',
+    schedule="@daily",
     catchup=False,
 ) as dag:
-    task1 = BashOperator(
-        task_id='start_task',
-        bash_command='echo "Start Task Running"'
+    hello_task = PythonOperator(
+        task_id="print_hello_task",
+        python_callable=print_hello,
     )
-
-    task2 = BashOperator(
-        task_id='end_task',
-        bash_command='echo "End Task Running"'
-    )
-
-    task1 >> task2
--- hello_airflow.py
+++ hello_airflow.py
@@ -3,12 +3,12 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id='hello_airflow',
-    start_date=datetime(2025, 7, 30),
-    schedule='@daily',
+    dag_id="new_hello_airflow",
+    start_date=datetime(2025, 8, 1),
+    schedule="@hourly",
     catchup=False,
 ) as dag:
-    task = BashOperator(
-        task_id='say_hello',
-        bash_command='echo "Hello from Airflow!"'
+    bash_task = BashOperator(
+        task_id="bash_hello_task",
+        bash_command='echo "This is a brand new hello from Airflow!"',
     )
