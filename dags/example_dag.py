61168a9a72971ea815d584bf18964f53ed379adf
Youssef Kassem <yousef.essa2112@gmail.com>
2025-08-05 05:38:57 -0400

    Cleaned up DAGs and updated airflow-values.yaml

--- dags/example_dag.py
+++
@@ -1,8 +0,0 @@
-from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from datetime import datetime
-
-with DAG("example_dag", start_date=datetime(2023, 1, 1), schedule_interval="@daily", catchup=False) as dag:
-    start = DummyOperator(task_id="start")
-    end = DummyOperator(task_id="end")
-    start >> end
